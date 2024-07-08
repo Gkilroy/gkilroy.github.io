af23e59f91a9876f277a83f0ad1a21f378e9daf9
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-08 15:03:34 -0700

    add css, html, and index javascript

---
+++ class-demo-projects/week4-demo-website-1/index.js
@@ -0,0 +1,33 @@
+//js es1...4
+function ex1() {
+
+    const output1 = document.getElementById('output1');
+
+    console.log("* ex1 js into");
+    console.log('*ex1 js into');
+    output1.innerText = "ex1 es1..es4 intro to js";
+    output1.innerText = 'ex1 es5 intro to js';
+
+
+}
+
+function ex2() {
+
+    const output1 = document.getElementById('output1');
+
+    console.log("* ex2 js into");
+    console.log('* ex2 js into');
+    output1.innerText = "ex2 es1..es4 intro to js";
+    output1.innerText = 'ex2 es5 intro to js';
+}
+
+function ex3() {
+    
+    const output1 = document.getElementById('output1');
+
+    console.log("* ex3 js into");
+    console.log('* ex3 js into');
+    output1.innerText = "ex3 es1..es4 intro to js";
+    output1.innerText = 'ex3 es5 intro to js';
+    return
+}
