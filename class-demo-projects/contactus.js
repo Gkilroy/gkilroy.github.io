c84d8de9521c07eb22efe129a23e2572b4ea023a
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-09 15:28:48 -0700

    saved files and pushed updates

--- class-demo-projects/contactus.js
+++ class-demo-projects/contactus.js
@@ -10,3 +10,21 @@ validation requirements
     required fields not empty
     text field max 30 characters
 */
+function validate_form(){
+var elename = document.getElementById("txtname");
+
+let elephone = document.getElementById("txtphone");
+
+const eleemail = document.getElementById("txtemail");
+
+const elecomment = document.getElementById("txtcomment");
+
+if(elecomment.value =="")
+    {
+        alert("* please enter a valid name")
+        return false;
+
+    }
+    
+    return true;
+}
