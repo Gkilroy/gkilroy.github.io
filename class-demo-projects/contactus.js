449748a552af353be55fac0dee4ce108cba6818d
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-09 14:32:03 -0700

    updated contact us page with form, changed IE edge to have an = sign instead of hypen

---
+++ class-demo-projects/contactus.js
@@ -0,0 +1,12 @@
+/*
+
+validation requirements
+------------------------------------
+- name 
+    required fields not empty
+    text field max 30 characters
+
+- email 
+    required fields not empty
+    text field max 30 characters
+*/
