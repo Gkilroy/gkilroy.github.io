1552be369d63ffa3794fdc5541c9e3baa42b1b54
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-12 15:47:08 -0700

    saved files and pushed

--- class-demo-projects/contactus.js
+++ class-demo-projects/contactus.js
@@ -282,3 +282,133 @@ function vlaidate_form5()
     //continue form submit
     return true;
 }
+function vlaidate_form6()
+{
+
+    var elename = document.getElementById("txtname");
+    let elephone = document.getElementById("txtphone");
+    const eleemail = document.getElementById("txtemail");
+    const elecomment = document.getElementById("txtcomment");
+
+    const ele_error_message = document.querySelector(".errorMessage");
+    //const ele_error_message = document.querySelector("#errorMessage");
+    //const ele_error_message = document.getElementById("errorMessage");
+
+    const ele_name_error = document.querySelector(".nameError");
+
+    const ele_form_error_list = document.querySelector(".form-Field-Error-List");
+    //const ele_form_error_list = document.querySelector("#form-Field-Error-List");
+    //const ele_form_error_list = document.getElementById("form-Field-Error-List");
+
+    let _form_error_array = [];
+    //let _form_error_array = new Array();
+
+    let _msg = "";
+
+    //phone is optional - no evaluate phone field
+
+    //* can also add/remove class - if 2 more classes
+    ele_name_error.style.display = "none";
+
+    //x === 5, validates that x and 5 are the same time ie: number then validates that x is equal to 5
+
+
+    //@@ optional refactor/optimization/code-reuse: create function
+    //
+    //validate_namel(elename.value) return true/false
+    //validate_form_fields('name', elename.value) return true/false
+    //
+    //if (validate_name(elename.value))
+    //
+
+    if (elename.value === "")
+    {
+        _msg = "* please enter a valid name";
+        _form_error_array.push(_msg);
+
+        ele_error_message.innerHTML = "<b>" + _msg  + "</b>";
+
+        //* can also add/remove class - if 2 more classes
+        ele_name_error.style.display = "block";
+        //return false;
+    }
+
+    //@@ optional refactor/optimization/code-reuse: create function
+    //
+    //validate_email(eleemail.value) return true/false
+    //validate_form_fields('email', eleemail.value) return true/false
+    //
+    //if (validate_email(eleemail.value))
+    //
+
+    //x==5, only validates that x is equal to 5
+    if (eleemail.value == "")
+    {
+        _msg = "* please enter a valid email";
+        _form_error_array.push(_msg);
+        ele_error_message.innerText = "<b>" +  _msg  + "</b>";
+
+        //return false;
+    }
+
+    if(_form_error_array.length>0){
+
+        //loop through array and display array items in the div
+
+        //@@ optional refactor/optimization/code-reuse: create function
+        //
+        //generate_field_error_list_string(_form_error_array) return string
+        //generate_field_error_list('string',_form_error_array) return string
+        //
+        // - unit test function seperat from application
+        //
+        //- html code string
+        /*
+        let _text = "<ul>";
+
+        for(let i=0; (i<_form_error_array.length);i++)
+        {
+            _text += "<li>" + _form_error_array[i] + "</li>";
+        }
+
+        _text += "</ul>";
+        */
+
+        let _text = generate_field_error_list_string(_form_error_array);
+        ele_form_error_list.innerHTML = _text;
+
+        //- dom element code
+        //
+        //generate_field_error_list_element(_form_error_array) return DocumentElement
+        //generate_field_error_list('element',_form_error_array) return DocumentElement
+        //
+        // - unit test function seperat from application
+        //
+        let ele_ul = document.createElement("ul");
+
+        for(let i=0; (i<_form_error_array.length);i++)
+        {
+            let ele_li = document.createElement("li");
+
+            //let ele_text = document.createTextNode(_form_error_array[i]);
+            let ele_p = document.createElement("p");
+            ele_p.innerText = _form_error_array[i];
+
+            //ele_li.appendChild(ele_text);
+            ele_li.appendChild(ele_p);
+            ele_ul.appendChild(ele_li);
+        }
+
+        //retrieve to html or text output from the ul
+        //ele_form_error_list.innerHTML = ele_ul.innerHTML;
+
+        //ele_form_error_list.innerHTML = "";
+        //ele_form_error_list.appendChild(ele_ul);
+
+        //stop submit button
+        return false;
+    }
+
+    //continue form submit
+    return true;
+}
