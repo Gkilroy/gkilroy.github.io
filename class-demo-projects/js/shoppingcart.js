65312496a966156b2c9e5eefd6ce6ea2ea475ebf
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-15 15:53:16 -0700

    update js shopping cart code

--- class-demo-projects/js/shoppingcart.js
+++ class-demo-projects/js/shoppingcart.js
@@ -1,76 +1,65 @@
-
-//bind to click event using dom.addEventListener
 document.getElementById("btnsearch").addEventListener("click", function(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
-
-        console.log("*btnsearch")
-        output1.innerText = "*btnsearch";
-
+    //can cancel or resume button click
+    //continue button click
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    console.log("* btnsearch")
+    output1.innerText = "* btnsearch ";
 });
-
 
 document.getElementById("btnclear").addEventListener("click", clearButton);
 
 function clearButton(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
-
-        let = _msg = "btnclear";
-
-        console.log(_msg)
-        output1.innerText = _msg;
-
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    let = _msg = "btnclear";
+    console.log(_msg)
+    output1.innerText = _msg;
 }
 
 document.getElementById("btnclose-addnew").addEventListener("click", function(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    const additemform = document.querySelector(".addnewitem");
 
-        _msg = "btnclose-addnew"
-        console.log(_msg)
-        output1.innerText = _msg;
+    _msg = "btnclose-addnew"
+    console.log(_msg)
+    output1.innerText = _msg;
 
-        additemform.style.display = "none";
-
-
+    additemform.style.display = "none";
 });
 
 document.getElementById("btnaddnew").addEventListener("click", function(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
-
-        _msg = "btnaddnew"
-        console.log(_msg)
-        output1.innerText = _msg;
-
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    _msg = "btnaddnew"
+    console.log(_msg)
+    output1.innerText = _msg;
 });
 
 document.getElementById("btnclear-addnew").addEventListener("click", function(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
-
-        _msg = "btnclear-addnew"
-        console.log(_msg)
-        output1.innerText = _msg;
-
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    _msg = "btnclear-addnew"
+    console.log(_msg)
+    output1.innerText = _msg;
 });
 
 document.getElementById("btnaddnewitem").addEventListener("click", function(e)
 {
-        e.preventDefault();
-        const output1 = document.getElementById("output1");
-        const additemform = document.querySelector(".addnewitem");
+    e.preventDefault();
+    const output1 = document.getElementById("output1");
+    const additemform = document.querySelector(".addnewitem");
 
-        _msg = "btnaddnewitem"
-        console.log(_msg)
-        output1.innerText = _msg;
+    _msg = "bntaddnewitem"
+    console.log(_msg)
+    output1.innerText = _msg;
 
-        additemform.style.display = "block";
+    additemform.style.display = "block";
 
 });
