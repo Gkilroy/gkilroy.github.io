f81ae7ccf2c8b8e7d58c053cf6e22cdddf7ba04f
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-16 09:49:48 -0700

    update shoppingcart array and object

--- class-demo-projects/js/shoppingcart.js
+++ class-demo-projects/js/shoppingcart.js
@@ -36,9 +36,43 @@ document.getElementById("btnaddnew").addEventListener("click", function(e)
 {
     e.preventDefault();
     const output1 = document.getElementById("output1");
+
+    const addnewmessage = document.getElementById("addnewitem-message");
+    const addname = document.getElementById("txtname-new");
+    const addprice = document.getElementById("txtprice-new");
+
+    let shoppingcart = [];
+
+    if (txtaddname.value === "")
+    {
+        const _msg = "*please enter a name";
+        addnewmessage.innerText = _msg;
+        e.stopImmediatePropagation();
+
+    }
+    else if(txtaddprice.value === "")
+    {
+        const _msg = "*please enter a price";
+        addnewmessage.innerText = _msg;
+        e.stopImmediatePropagation();
+    }
+ 
+    const _price = parseFloat(txtaddprice.value);
+
+    var_cartitem = {name:textaddname, price:_price};
+
+    _shoppingcart.push(_cartitem);
+
+    for(let i=0;i<_shoppingcart.length;i++)
+    {
+        output1.innerHTML = output1.innerHTML + _shoppingcart[i] + "<br/>"
+        _total = _total + _price;
+    }
+
     _msg = "btnaddnew"
     console.log(_msg)
-    output1.innerText = _msg;
+    //output1.innerText = _msg;
+
 });
 
 document.getElementById("btnclear-addnew").addEventListener("click", function(e)
