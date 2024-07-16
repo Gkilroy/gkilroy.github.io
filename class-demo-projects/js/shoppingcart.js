8810bedd3295deb57a3d0093ea213228bc3d90e4
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-16 10:46:53 -0700

    update shoppingcart js

--- class-demo-projects/js/shoppingcart.js
+++ class-demo-projects/js/shoppingcart.js
@@ -40,8 +40,14 @@ document.getElementById("btnaddnew").addEventListener("click", function(e)
     const txtaddname = document.getElementById("txtname-new");
     const txtaddprice = document.getElementById("txtprice-new");
 
+    //-- javascript local scope, global scope
+    //-- Application Refactor
+    //-- application global data store /state/ ie: cookies, localstorage, sessionstorage, indexdb, react/redux
+    //-- BrowserDev Tools to provew application date/state
+    // create shopping cart array
     let _shoppingcart = [];
 
+    //validate only add max 10 items
 
     if(txtaddname.value === "")
     {
@@ -58,17 +64,22 @@ document.getElementById("btnaddnew").addEventListener("click", function(e)
 
     //convert string to number - parseFloat()
     const _price = parseFloat(txtaddprice.value);
+    //format number to fixed to 2 decimal places, _price.toFixed(2)
+    //validate number isNan() - ie: abc, 1ab
+    //error handling
 
+    //create a new shoppingcart entry/object ie: {name, price}
+    //javascript object = json document, NOSQL ie: mongodb and other systems
     var _cartitem = {name:txtaddname.value, price:_price};
 
-    
+    //add shoppingcart entry/object to ul/list
     _shoppingcart.push(_cartitem);
 
     let _total = 0.0;
-    
+    //update ui/li with cart item
     for(let i=0;i<_shoppingcart.length;i++)
     {
-   
+        //use Dom.CreateElement, appendChild
         output1.innerHTML = output1.innerHTML + JSON.stringify(_shoppingcart[i]) + "<br/>";
         _total = _total + _price;
     }
