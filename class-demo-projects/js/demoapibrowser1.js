46ff36492c392a2e546175b3a14cc538d856516a
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-19 13:38:50 -0700

    update demo api js

--- class-demo-projects/js/demoapibrowser1.js
+++ class-demo-projects/js/demoapibrowser1.js
@@ -1,4 +1,4 @@
-document.getElementById("btnsearch1").addEventListener("click",function(e) {
+document.getElementById("btnsearch1").addEventListener("click",async function(e) {
 
     const output1 = document.getElementById("output1");
     const txtsearch = document.querySelector("#txtsearch");
@@ -21,7 +21,56 @@ document.getElementById("btnsearch1").addEventListener("click",function(e) {
             searchmessage.innerText = `* searching for: ${txtsearch.value}`;
         }
     
-        output1.innerText = _function_name;
+        //output1.innerText = _function_name;
+
+        const _url1 = "./data/giphy1.json";
+
+        //https://developers.giphy.com/docs/api/
+        //https://developers.giphy.com/explorer/
+
+        const _giphy_api_key = "giphy-key";
+
+        const _gifyapiurl = 'https://api.giphy.com/v1/gifs/search?';
+        const _apiKey = `&api_key=${_giphy_api_key}`;
+        const _query = `&q=${txtsearch.value}`;
+
+        //other giphy parameters
+        //- limit , rating, etc..
+
+        const _url = `${_gifyapiurl}${_apiKey}${_query}`;
+
+        searchmessage.innerText = `${_url}`;
+
+        //output1.innerText = _url;
+
+        //promise.then() - non nested api request 
+
+        //async/await - multi/nested api , multiple api calls 
+
+        //disply content - for, forEach, map, filter, other         
+        //display max ie: 10 results 
+
+        //-- html string or dom createelement, appendchile...
+
+        fetch(_url1)
+        .then(response => response.json())
+        .then(data => {
+    
+           //let _img_list = "<div style='display:flex'>";
+           let _img_list = "<div>";
+
+           data.data.forEach((item,index) => {
+
+            if(index <= 4)
+                _img_list += `<div><img src="${item.images.original.url}" width="200" height="80"></div>`;
+           });
+
+           _img_list += "</div>";
+
+           output1.innerHTML = _img_list;
+
+        });
+
 
     } catch (error) {
         output1.innerText = `${_function_name}::error ${error}`;
@@ -75,6 +124,7 @@ document.getElementById("btnclear").addEventListener("click",function(e) {
         e.preventDefault();
 
         searchmessage.innerText = "";
+        txtsearch.value = "";
 
         //output1.innerText = _function_name;
 
@@ -187,13 +237,37 @@ document.getElementById("btnfetchex1").addEventListener("click",function(e) {
            const _image_url = _row.images.original.url;
 
            //output1.innerText = JSON.stringify(_row);
-           output1.innerText = _image_url;
+           //output1.innerText = _image_url;
+
+           //const _img = `<div><img src="${_image_url}" width="200" height="80"></div>`;
+
+           let _img_list = "";
+
+           data.data.forEach((item,index) => {
+
+            if(index <= 4)
+                _img_list += `<div><img src="${item.images.original.url}" width="200" height="80"></div>`;
+
+                /*
+                if(index > 4){
+                    return
+                }
+                */
+                
+                //if(index > 4)  return;
+                //console.log(`** foreach.index: ${index}`)
+                if(index > 4) 
+                    return false;
+           });
+
+           output1.innerHTML = _img_list;
 
             //
             //convert data to json object - JSON.parse(data)
             //
             //for, forEach, map, find, filter, other
             //
+
         });
 
         /*
