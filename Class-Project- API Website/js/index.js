4f8da80069cc1011ed199475a3556b6a6f275f7e
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-07-22 16:43:48 -0700

    create index html and js

---
+++ Class-Project- API Website/js/index.js
@@ -0,0 +1,66 @@
+function closenavmenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+document.getElementById("btnsearch").addEventListener("click",async function(e) {
+
+    const divcontent = document.getElementById("divcontent");
+    const txtsearch = document.querySelector("#txtsearch");
+    const searchmessage = document.querySelector(".searchmessage");
+
+    const _function_name = "btnsearch";
+
+    try 
+    {
+        
+        e.preventDefault();
+
+        if(txtsearch.value === ""){
+            searchmessage.innerText = "* please enter search keyword(s)"
+            return false;
+        }
+        else {
+            searchmessage.innerText = `* searching for: ${txtsearch.value}`;
+        }
+    
+        const _url1 = "./data/giphy1.json";
+
+        const _giphy_api_key = "giphy-key";
+
+        const _gifyapiurl = 'https://api.giphy.com/v1/gifs/search?';
+        const _apiKey = `&api_key=${_giphy_api_key}`;
+        const _query = `&q=${txtsearch.value}`;
+
+        const _url = `${_gifyapiurl}${_apiKey}${_query}`;
+        
+        fetch(_url1)
+        .then(response => response.json())
+        .then(data => {
+    
+           divcontent.innerHTML = "";
+
+           //-- refactor: html string or dom createelement, appendchile...
+           let _img_list = "";
+
+           data.data.forEach((item,index) => {
+
+                if(index <= 4)
+                    _img_list += `<div class='imagediv'><img src="${item.images.original.url}" width="200" height="100"></div>`;
+
+            });
+            
+            divcontent.innerHTML = _img_list;
+        });
+        
+
+
+    } catch (error) {
+        output1.innerText = `${_function_name}::error ${error}`;
+    }
+
+});
