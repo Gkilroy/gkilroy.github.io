b805ff9dabc40f44340ea45004007b6a082e21f4
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-27 14:54:11 -0700

    update index.js

--- demo-week7-node-apps/node-express-demo-webserver-v1/index.js
+++ demo-week7-node-apps/node-express-demo-webserver-v1/index.js
@@ -1,26 +1,31 @@
 const express = require("express")
 const app = express();
 
-const _PORT =8080;
-
-const _msg = `Node/express webserver 1.0.0 ...running on port ${_PORT}`;
-
-app.get("/",(req,res)=>{
-    res.send("Node/Express webserver v1.0.0");
+//-- routes -- 
+//-- GET requesst -- 
+app.get("/",(req, res) =>{
+    //response 
+    const _msg = "Node/Express webserver v1.0.1";
+    console.log(_msg)
+    res.send(_msg);
 });
 
-
-app.get("/test1",(req,res)=>{
+app.get("/test1",(req, res) =>{
     res.send("Node/Express test1 router");
 });
 
-
-app.get("/login",(req,res)=>{
+app.get("/login",(req, res) =>{
     res.send("Node/Express login router");
 });
 
+//available localhost ports rang 3000 - 9000
+const _PORT = 8080;
+
+const _msg = `Node/express webserver 1.0.0 ...running on port: ${_PORT}`;
+
+//-- continiously run server listeing at port 8080
 app.listen(_PORT, ()=>{
-    console.log(_msg)
+    console.log(_msg);
 });
 
 
