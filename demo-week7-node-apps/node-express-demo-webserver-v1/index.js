0a2726c64683f9bc5556f9fbe07c2395f60d72ff
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-27 14:43:49 -0700

    update index.js and test node

--- demo-week7-node-apps/node-express-demo-webserver-v1/index.js
+++ demo-week7-node-apps/node-express-demo-webserver-v1/index.js
@@ -1,8 +1,26 @@
-function main ()
-{
-    const _msg = "Node Express web server 1.0.0";
-    console.log(_msg);
-}
+const express = require("express")
+const app = express();
 
-main();
+const _PORT =8080;
+
+const _msg = `Node/express webserver 1.0.0 ...running on port ${_PORT}`;
+
+app.get("/",(req,res)=>{
+    res.send("Node/Express webserver v1.0.0");
+});
+
+
+app.get("/test1",(req,res)=>{
+    res.send("Node/Express test1 router");
+});
+
+
+app.get("/login",(req,res)=>{
+    res.send("Node/Express login router");
+});
+
+app.listen(_PORT, ()=>{
+    console.log(_msg)
+});
+
 
